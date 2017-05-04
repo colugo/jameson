40a6a6ba69e8b355d28902b6d7eb836bd851e8ac
colugo <alastair.parker@gmail.com>
2017-05-04 11:36:57 +1000

    Cleanup

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
-
-
-<img src="https://gitcdn.xyz/cdn/colugo/jameson/943a2f95571154414ba326b4f165e4fe2ce5562f/build/icons/512x512.png" alt="logo" width="200px">
-
 # Jameson Copy Editor
-
 **Editing your prose so you don't have to**
 
+![logo](https://gitcdn.xyz/cdn/colugo/jameson/943a2f95571154414ba326b4f165e4fe2ce5562f/build/icons/512x512.png =250x)
 
  - Write your prose using markdown
- - Jameson will highlight sentences that are:
-     - too complicated
-     - way too complicated
+ - Jameson will highlight:
+     - Sentences that are too complicated - yellow
+     - Sentences that way too complicated - red
+     - Spelling - deep red
+     - Plain English alternatives ( from the [GOV.AU content guide](http://content-guide.apps.staging.digital.gov.au/writing-style/) ) - purple
 # License
 
 Released under the MIT license.
--- app/js/readabilityHandler.js
+++ app/js/readabilityHandler.js
@@ -7,29 +7,16 @@ function setupReadabilityHandler(){
     numberOfSentences = 1;
   }
   var wordCount = $('.words').text();
-  if(wordCount == 0){
-    wordCount = 1;
+
+  if(wordCount > 0){
+    var syllables = syllable(workingText);
+    var score = 0.39 * (wordCount / numberOfSentences) + 11.8 * (syllables / wordCount) - 15.59;
+    score = score.toFixed();
   }
-  var syllables = syllable(workingText);
-/*
-  var score = 206.835 - 1.015 * (wordCount / numberOfSentences) - 84.6 * (syllables / wordCount)
-
-  var grade = "College Graduate";
-  if(score > 30) grage = "College";
-  if(score > 50) grade =  "10th to 12th";
-  if(score > 60) grade = "8th & 9th";
-  if(score > 70) grade = "7th";
-  if(score > 80) grade = "6th";
-  if(score > 90) grade = "5th";
-
-  console.log("Reading level = " + grade);
-*/
-  var score = 0.39 * (wordCount / numberOfSentences) + 11.8 * (syllables / wordCount) - 15.59;
-  if(score < 0){
-     score = "-";
-   }else{
-     score = score.toFixed();
-   }
+  else{
+    score = "";
+    wordCount = "";
+  }
   $('.readability').text(score);
   $('.wordCount').text(wordCount);
 }
