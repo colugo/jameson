1293bb0cc899c087f2494c78208e86339a115f3d
colugo <alastair.parker@gmail.com>
2017-05-24 09:19:27 +1000

    Highlighting to status bar

--- app/js/readabilityHandler.js
+++ app/js/readabilityHandler.js
@@ -12,11 +12,28 @@ function setupReadabilityHandler(){
     var syllables = syllable(workingText);
     var score = 0.39 * (wordCount / numberOfSentences) + 11.8 * (syllables / wordCount) - 15.59;
     score = score.toFixed();
+    avgWords = Math.round(wordCount / numberOfSentences);
+    addColour(score, avgWords);
   }
   else{
     score = "";
     wordCount = "";
+    avgWords = "";
   }
   $('.readability').text(score);
   $('.wordCount').text(wordCount);
+  $('.avgWords').text(avgWords);
+}
+
+function addColour(score, avgWords){
+  if(avgWords <= 20)$('.avgWords').css('background-color','inherit');
+  if(avgWords > 20)$('.avgWords').css('background-color','rgba(255, 255, 0, 0.15)');
+  if(avgWords > 30)$('.avgWords').css('background-color','rgba(255, 0, 0, 0.15)');
+
+  if(score <= 5)$('.readability').css('background-color','inherit');
+  if(score > 8)$('.readability').css('background-color','rgba(249, 215, 0, 0.15)');
+  if(score > 11)$('.readability').css('background-color','rgba(235, 164, 0, 0.15)');
+  if(score > 14)$('.readability').css('background-color','rgba(220, 119, 0, 0.15)');
+  if(score > 19)$('.readability').css('background-color','rgba(200, 79, 0, 0.15)');
+  if(score > 21)$('.readability').css('background-color','rgba(255, 0, 0, 0.15)');
 }
